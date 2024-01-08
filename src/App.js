3f0957a22c318d975fceb53f9eca96c0d55bff9b
Monisha Dhanasekar <monishad@mediguru.io>
2024-01-08 12:15:55 +0530

    Added some design to forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
 import React, { useState, useEffect } from 'react';
+import { numberToWords } from "amount-to-words";
+import './App.css';
 
 function App() {
   const [basicSalary, setBasicSalary] = useState('');
   const [dearnessAllowance, setDearnessAllowance] = useState('');
   const [hra, setHra] = useState('');
   const [totalRent, setTotalRent] = useState('');
-  const [isMetrocity, setIsMetrocity] = useState();
+  const [isMetrocity, setIsMetrocity] = useState(true);
   const [perofsalary, setPerofsalary] = useState('');
   const [rent, setRent] = useState('');
   const [exemptedhra, setExemptedhra] = useState('');
+  const [highlightedInput, setHighlightedInput] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,61 +27,149 @@ function App() {
   }
 
   useEffect(() => {
-    if (isMetrocity) {
-      const add = parseInt(basicSalary) + parseInt(dearnessAllowance);
-      const salary = 0.5 * (parseInt(basicSalary) + parseInt(dearnessAllowance));
-      setPerofsalary(salary);
+    if (basicSalary && dearnessAllowance && totalRent) {
+      const numericBasicSalary = parseFloat(basicSalary.replace(/,/g, '')) || 0;
+      const numericDearnessAllowance = parseFloat(dearnessAllowance.replace(/,/g, '')) || 0;
+      const numericTotalRent = parseFloat(totalRent.replace(/,/g, '')) || 0;
+      const numericHRA = parseFloat(hra.replace(/,/g, '')) || 0;
+
+      console.log("console", numericBasicSalary, numericDearnessAllowance, numericTotalRent, numericHRA);
+
+      let salary;
+      if (isMetrocity) {
+        salary = 0.5 * (numericBasicSalary + numericDearnessAllowance);
+      } else {
+        salary = 0.4 * (numericBasicSalary + numericDearnessAllowance);
+      }
+
+      setPerofsalary(salary.toLocaleString('en-IN'));
+
+      const rentValue = numericTotalRent - 0.1 * (numericBasicSalary + numericDearnessAllowance);
+      setRent(Math.max(0, rentValue).toLocaleString('en-IN'));
+
+      const exehra = Math.min(numericHRA, rentValue, salary);
+      console.log("values of execha", salary, rentValue, numericHRA);
+      console.log("exehra", exehra);
+      setExemptedhra(Math.max(0, exehra).toLocaleString('en-IN'));
+      setHighlightedInput(exehra === salary ? 'basicSalary' : exehra === rentValue ? 'totalRent' : 'hra');
+
+      setHra(numericHRA.toLocaleString('en-IN'));
+      console.log("format numbers", basicSalary, dearnessAllowance, hra, totalRent);
     }
-    if (isMetrocity == false) {
-      const salary = 0.4 * (parseInt(basicSalary) + parseInt(dearnessAllowance));
-      setPerofsalary(salary);
+  }, [basicSalary, dearnessAllowance, hra, totalRent, isMetrocity, perofsalary]);
+
+  console.log("hra", typeof (hra));
+
+  // function formatNumber(number) {
+  //   const parsedNumber = parseFloat(number.replace(/,/g, '')) || '';
+  //   if (isNaN(parsedNumber)) {
+  //     return '';
+  //   }
+  //   return parsedNumber.toLocaleString('en-IN');
+  // }
+
+  const handleInputChange = (value, setValue) => {
+    const numericValue = parseFloat(value.replace(/,/g, '')) || 0;
+    if (!isNaN(numericValue)) {
+      setValue(numericValue.toLocaleString('en-IN'));
     }
-    if (totalRent) {
-      const rent = totalRent - (0.1 * (parseInt(basicSalary) + parseInt(dearnessAllowance)));
-      setRent(rent);
+    else {
+      setValue('');
     }
-    if (perofsalary && rent && hra) {
-      const exehra = Math.min(perofsalary, rent, hra);
-      setExemptedhra(exehra);
-    }
-  }, [basicSalary, dearnessAllowance, hra, totalRent, isMetrocity, perofsalary, rent])
+  };
 
   return (
     <>
+      <h2 className="header">HRA Calculator</h2>
       <div className="container">
         <div className='row'>
           <div className='column'>
             <form className="form">
-              <h2 className="header">HRA Calculator</h2>
-              <label>Basic salary recieved (per annum)</label>
-              <input className="input" type="number" placeholder="Enter Basic salary" onChange={(e) => setBasicSalary(e.target.value)} />
-              <label>Dearness Allowance recieved</label>
-              <input className="input" type="number" placeholder="Enter Dearness allowance" onChange={(e) => setDearnessAllowance(e.target.value)} />
-              <label>HRA recieved (per annum)</label>
-              <input className="input" type="number" placeholder="Enter HRA" onChange={(e) => setHra(e.target.value)} />
+              <label>Basic salary received (per annum)</label>
+              <input
+                className="input"
+                type="text"
+                placeholder="Enter Basic salary"
+                value={basicSalary}
+                onChange={(e) => setBasicSalary(e.target.value)}
+                onBlur={() => handleInputChange(basicSalary, setBasicSalary)}
+              />
+              {basicSalary && (
+                <p className='words'>{numberToWords(parseFloat(basicSalary.replace(/,/g, '')))}</p>
+              )}
+              <label>Dearness Allowance received</label>
+              <input
+                className="input"
+                type="text"
+                placeholder="Enter Dearness allowance"
+                value={dearnessAllowance}
+                onChange={(e) => setDearnessAllowance(e.target.value)}
+                onBlur={() => handleInputChange(dearnessAllowance, setDearnessAllowance)}
+              />
+              {dearnessAllowance && (
+                <p className='words'>{numberToWords(parseFloat(dearnessAllowance.replace(/,/g, '')))}</p>
+              )}
+              <label>HRA received (per annum)</label>
+              <input
+                className="input"
+                type="text"
+                placeholder="Enter HRA"
+                value={hra}
+                onChange={(e) => setHra(e.target.value)}
+                onBlur={() => handleInputChange(hra, setHra)}
+              />
+              {hra && (
+                <p className='words'>{numberToWords(parseFloat(hra.replace(/,/g, '')))}</p>
+              )}
               <label>Total rent paid</label>
-              <input className="input" type="number" placeholder="Enter rent" onChange={(e) => setTotalRent(e.target.value)} />
-              <p>Do you live in metro city?(Delhi, Mumbai, Kolkata, Chennai)</p>
+              <input
+                className="input"
+                type="text"
+                placeholder="Enter rent"
+                value={totalRent}
+                onChange={(e) => setTotalRent(e.target.value)}
+                onBlur={() => handleInputChange(totalRent, setTotalRent)}
+              />
+              {totalRent && (
+                <p className='words'>{numberToWords(parseFloat(totalRent.replace(/,/g, '')))}</p>
+              )}
+              <p>Do you live in a metro city?(Delhi, Mumbai, Kolkata, Chennai)</p>
               <div>
-                <input type="radio" id="city1" value="yes" name="city" onChange={(e) => setIsMetrocity(true)}></input>
+                <input type="radio" id="city1" value="yes" name="city" onChange={() => setIsMetrocity(true)} />
                 <label htmlFor="city1">Yes</label>
-                <input type="radio" id="city2" value="no" name="city" onChange={(e) => setIsMetrocity(false)}></input>
+                <input type="radio" id="city2" value="no" name="city" onChange={() => setIsMetrocity(false)} />
                 <label htmlFor="city2">No</label>
               </div>
-
+              <button className='calbtn' onClick={handleSubmit}>Clear form</button>
             </form>
           </div>
           <div className='column'>
             <form className="form1">
               <label>Actual HRA Received</label>
-              <input className="input1" type="number" value={hra} readOnly />
-              <label>Rent paid in excess of 10% of salary</label>
-              <input className="input1" type="number" value={rent} readOnly />
-              {isMetrocity ? <label>40% of Basic salary(For those living in metro cities)</label> : <label>50% of Basic salary(For those living in non metro cities)</label>}
-              <input className="input1" type="number" value={perofsalary} readOnly />
-              <label>Amount of Exempted HRA(Actual rent paid minus 10% of (Basic salary + Dearness allowance))</label>
-              <input className="input1" type="number" value={exemptedhra} readOnly />
-              <button className='calbtn' onClick={handleSubmit}>Calculate Again</button>
+
+              <input className={`input1 ${highlightedInput === 'hra' ? 'highlighted' : ''}`} type="text" value={hra} readOnly />
+              {hra && (
+                <p className='words'>{numberToWords(parseFloat(hra.replace(/,/g, '')))}</p>
+              )}
+              <label>Rent paid in excess of 10% of salary (Actual rent paid minus 10% of (Basic salary + Dearness allowance))</label>
+              <input className={`input1 ${highlightedInput === 'totalRent' ? 'highlighted' : ''}`} type="text" value={rent} readOnly />
+              {rent && (
+                <p className='words'>{numberToWords(parseFloat(rent.replace(/,/g, '')))}</p>
+              )}
+
+              {isMetrocity ? <label>50% of Basic salary (For those living in metro cities)</label> : <label>40% of Basic salary (For those living in non-metro cities)</label>}
+              <input className={`input1 ${highlightedInput === 'basicSalary' ? 'highlighted' : ''}`} type="text" value={perofsalary} readOnly />
+              {perofsalary && (
+                <p className='words'>{numberToWords(parseFloat(perofsalary.replace(/,/g, '')))}</p>
+              )}
+              <hr></hr>
+              <div className='output'>
+                <label>Amount of Exempted HRA</label>
+                <input className="input1" type="text" value={exemptedhra} readOnly />
+                {exemptedhra && (
+                  <p className='words1'>{numberToWords(parseFloat(exemptedhra.replace(/,/g, '')))}</p>
+                )}
+              </div>
             </form>
           </div>
         </div>
